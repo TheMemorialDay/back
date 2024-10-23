e53bf44b3e20a568d0f4c32ca13321b211e63db8
pdu08075 <rpaeheh@naver.com>
2024-10-23 13:12:18 +0900

    the memorial day back first commit

---
+++ src/main/java/com/korit/thememorialday/ThememorialdayApplication.java
@@ -0,0 +1,13 @@
+package com.korit.thememorialday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThememorialdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThememorialdayApplication.class, args);
+	}
+
+}
