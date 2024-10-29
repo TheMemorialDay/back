e7befc7faf807841dc04b41a052c75626ea730d6
ristukaJJang <ghwjd5585@gmail.com>
2024-10-29 16:32:58 +0900

    complete join function

--- src/main/java/com/korit/thememorialday/service/join/JoinService.java
+++ src/main/java/com/korit/thememorialday/service/join/JoinService.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import com.korit.thememorialday.dto.request.join.PatchJoinRequestDto;
 import com.korit.thememorialday.dto.response.ResponseDto;
 
-
 public interface JoinService {
 
     // 아이디를 통해 회원을 확인하고 사업자 등록 정보 입력 받음
