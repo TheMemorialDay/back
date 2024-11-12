e13deb0decc6d3a2060b949e0771bb0dc97281f5
pdu08075 <rpaeheh@naver.com>
2024-11-12 14:19:51 +0900

    back 2024.11.12.14.19

--- src/main/java/com/korit/thememorialday/service/implement/order/OrderServiceImplement.java
+++ src/main/java/com/korit/thememorialday/service/implement/order/OrderServiceImplement.java
@@ -52,10 +52,10 @@ public class OrderServiceImplement implements OrderService {
             List<OrderSelectOptionEntity> optionEntities = new ArrayList<>();
             for (OrderSelectOption orderSelectOption : orderSelectOptions) {
 
-                // Integer optionNumber = orderSelectOption.getOptionNumber();
+                Integer optionNumber = orderSelectOption.getOptionNumber();
                 Integer optionCategoryNumber = orderSelectOption.getOptionCategoryNumber();
 
-                OrderSelectOptionEntity optionEntity = new OrderSelectOptionEntity(orderCode, optionCategoryNumber); 
+                OrderSelectOptionEntity optionEntity = new OrderSelectOptionEntity(orderCode, optionNumber, optionCategoryNumber); 
                 optionEntities.add(optionEntity);
             }
             orderSelectOptionRepository.saveAll(optionEntities);
@@ -107,6 +107,7 @@ public class OrderServiceImplement implements OrderService {
         }
 
         List<String> orderCodes = orders.stream().map(OrderEntity::getOrderCode).collect(Collectors.toList());
+
         List<OrderSelectOptionEntity> optionEntities = orderSelectOptionRepository.findByOrderCodeIn(orderCodes);
 
         Map<Integer, String> productCategoryMap = optionEntities.stream()
@@ -122,6 +123,7 @@ public class OrderServiceImplement implements OrderService {
                 Collectors.mapping(
                     option -> new OrderSelectOption(
                         option.getOptionCategoryNumber(),
+                        option.getOptionNumber(),
                         productCategoryMap.get(option.getOptionCategoryNumber())),
                     Collectors.toList())));
 
